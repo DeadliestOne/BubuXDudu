cca6d8575e269396473b91b34b477ac093fece2f
DeadliestOne <uncountableaura@gmail.com>
2024-10-09 08:45:06 +0530

    Update Youtube.py

--- BubuxDudu/platforms/Youtube.py
+++ BubuxDudu/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from BubuXDudu.utils.database import is_on_off
-from BubuXDudu.utils.formatters import time_to_seconds
+from BubuxDudu.utils.database import is_on_off
+from BubuxDudu.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
